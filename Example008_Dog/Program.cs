51a44b07c6e0e952d90c24d56ef64b5dd18edf63
Nika <nikarasevas@gmail.com>
2022-09-15 00:32:46 +0300

    Задача про собачку

---
+++ Example008_Dog/Program.cs
@@ -0,0 +1,29 @@
+﻿int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+
+while (distance > 10);
+{
+    int time;
+    if (friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else 
+    {
+       time = distance / (secondFriendSpeed + dogSpeed);
+       friend = 1; 
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+}
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.WriteLine("раз.");
+
+
+
